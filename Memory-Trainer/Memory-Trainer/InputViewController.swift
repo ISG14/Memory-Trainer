c86ffa5736fb6f012769ea422001ca376d4c86e0
Rio Lynk <kinglynk13@gmail.com>
2017-06-11 23:06:09 -0400

    Picker View + num pad

--- Memory-Trainer/Memory-Trainer/InputViewController.swift
+++ Memory-Trainer/Memory-Trainer/InputViewController.swift
@@ -8,27 +8,43 @@
 
 import UIKit
 
-class InputViewController: UIViewController {
+class InputViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     //VARIABLES
-    
+    var pickerDataSource = ["Test"]
     
     //OUTLETS
     @IBOutlet weak var leftTextField: UITextField!
     @IBOutlet weak var middleTextField: UITextField!
     @IBOutlet weak var rightTextField: UITextField!
+    @IBOutlet weak var inputPickerView: UIPickerView!
     
     
     //ACTIONS
     
     
     //FUNCTIONS
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //test
+        leftTextField.keyboardType = UIKeyboardType.numberPad
+        middleTextField.keyboardType = UIKeyboardType.numberPad
+        rightTextField.keyboardType = UIKeyboardType.numberPad
+        
         //Move text fields out of view to animate in
         leftTextField.center.x -= 150
         rightTextField.center.x += 150
@@ -40,6 +56,10 @@ class InputViewController: UIViewController {
             self.rightTextField.center.x -= 150
             self.middleTextField.center.y -= 400
         }
+        
+        //test
+        self.inputPickerView.dataSource = self;
+        self.inputPickerView.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
